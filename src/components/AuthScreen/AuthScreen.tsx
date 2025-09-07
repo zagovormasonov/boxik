b573ae82210b8d73a2d874650079e8bc6dadf48f
noName <samyrize77777@gmail.com>
2025-09-07 07:45:26 +0400

    deploy

--- src/components/AuthScreen/AuthScreen.tsx
+++ src/components/AuthScreen/AuthScreen.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import AuthForm from '../AuthForm/AuthForm'
 import YandexAuth from '../YandexAuth/YandexAuth'
