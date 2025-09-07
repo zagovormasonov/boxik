1ebe8ef06deecb2f1ed6c563c8dd1f0c6d412c47
noName <samyrize77777@gmail.com>
2025-09-07 23:53:04 +0400

    deploy

--- src/components/TestScreen/TestScreen.tsx
+++ src/components/TestScreen/TestScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useTest } from '../../contexts/TestContext'
 import { useAuth } from '../../contexts/AuthContext'
