86a8e409fe628df6d4722ece041e1a7fc5c41236
noName <samyrize77777@gmail.com>
2025-09-10 12:01:15 +0400

    fix css

--- src/components/TestLanding/TestLanding.tsx
+++ src/components/TestLanding/TestLanding.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Brain, Shield, FileText, Clock, CheckCircle } from 'lucide-react'
+import image1 from '../../img/1.png'
+import image2 from '../../img/2.png'
+import image3 from '../../img/3.png'
 
 const TestLanding: React.FC = () => {
   const navigate = useNavigate()
@@ -12,19 +15,19 @@ const TestLanding: React.FC = () => {
   const features = [
     {
       icon: <Brain size={24} />,
-      image: '/src/img/1.png',
+      image: image1,
       title: 'Профессиональная диагностика',
       description: 'Тест основан на официальных критериях DSM-5 для диагностики пограничного расстройства личности'
     },
     {
       icon: <Shield size={24} />,
-      image: '/src/img/2.png',
+      image: image2,
       title: 'Конфиденциальность',
       description: 'Все ваши ответы строго конфиденциальны и не передаются третьим лицам'
     },
     {
       icon: <FileText size={24} />,
-      image: '/src/img/3.png',
+      image: image3,
       title: 'Детальный отчет',
       description: 'Получите подробный анализ результатов с рекомендациями специалистов'
     },
