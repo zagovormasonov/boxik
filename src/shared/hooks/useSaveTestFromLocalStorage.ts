57b01988baa2f074d38b91f166bc2123f5524142
noName <samyrize77777@gmail.com>
2025-09-10 16:57:38 +0400

    payment

--- src/shared/hooks/useSaveTestFromLocalStorage.ts
+++ src/shared/hooks/useSaveTestFromLocalStorage.ts
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { supabase } from '../lib/supabase'
+import { supabase } from '../../lib/supabase'
 import { BPDTestState, BPDSeverity } from '../../types'
 
 interface SaveTestFromLocalStorageParams {
